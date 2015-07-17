61fa49e0ff549b86557a81d18202a47b55241445
Stefan Wallentowitz <stefan@simless.com>
2015-07-17 11:36:12 +0200

    Router: Add license and fix documentation

--- src/scala/RingRouter.scala
+++ src/scala/RingRouter.scala
@@ -1,3 +1,29 @@
+/* Copyright (c) 2015 by the author(s)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * =============================================================================
+ *
+ * Author(s):
+ *   Stefan Wallentowitz <stefan@wallentowitz.de>
+ */
+
 package OpenSoCDebug
 
 import Chisel._
@@ -10,13 +36,15 @@ class NetworkLink (data_width: Int)
 /** Ring Router
   * 
   * This is the basic building block for the debug interconnect
-  * ring. The router is very straight-forward and is designed for
-  * deadlock-free rings: It has two ring paths to build a circle-free
-  * channel dependency diagram (spiral). In the ring the input and
-  * output ids are simply connected (router(i).out0 <> router(i+1).in0
-  * and router(i).out1 <> router(i+1).in1). After the last router, the
-  * inner ring (0) is connected (router(N-1).out0 <> router(0).in1)
-  * and the remaining input and output are unconnected.
+  * ring. It is required to set the router identifier on the io bundle
+  * (allows to use the router as verilog module). The router is very
+  * straight-forward and is designed for deadlock-free rings: It has
+  * two ring paths to build a circle-free channel dependency diagram
+  * (spiral). In the ring the input and output ids are simply
+  * connected (router(i).out0 <> router(i+1).in0 and router(i).out1 <>
+  * router(i+1).in1). After the last router, the inner ring (0) is
+  * connected (router(N-1).out0 <> router(0).in1) and the remaining
+  * input and output are unconnected.
   *
   * The router is output buffered and the buffer size is
   * parametrizable. It also has an ID and all packets with the
